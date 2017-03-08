0d1bc3f167f9db53397285a293f7615a22c86271
azzazelo <azzazel@unseen.is>
2017-03-08 20:08:02 +0100

    Ajax Requests with React

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBeLPsoMydp_DzZJEPUhzKC6VxKJzdJoPU'; 
 
@@ -20,6 +21,8 @@ class App extends Component{
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
