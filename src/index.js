65592b9b52eac2e2b6e2d7c07576c28c53d1ac43
azzazel <azzazel@gmail.com>
2017-03-09 10:52:18 +0100

    s

--- src/index.js
+++ src/index.js
@@ -30,8 +30,8 @@ class App extends Component{
   }
   render() {
     
-    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
-    
+    const videoSearch = _.debounce((term) => {this.cv(term)}, 300);
+
     return (
       <div>
         <SearchBar onSearchTermChange={videoSearch}/>
